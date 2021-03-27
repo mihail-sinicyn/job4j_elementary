c1739c1c84bd66f0a7c067dd7a239fc9eec1402e
mihail-sinicyn <sinicyn.mihail@gmail.com>
2021-03-28 00:07:48 +0300

    4.1. Аргументы [#503599]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Mihail Sinitsyn");
+        System.out.println("21.05.1986");
+    }
+}
