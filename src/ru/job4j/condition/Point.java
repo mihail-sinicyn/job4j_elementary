7bc904d3ae9aa128cbffe12641f278a65618f055
mihail-sinicyn <sinicyn.mihail@gmail.com>
2021-05-26 01:50:40 +0300

    4. Расстояние между точками в трехмерном пространстве. [#122643]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -9,19 +9,35 @@ public class Point {
 
     private int y;
 
+    private int z;
+
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
+    }
+
+    public Point(int first, int second, int third) {
+        this.x = first;
+        this.y = second;
+        this.z = third;
     }
 
     public double distance(Point that) {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
+    }
+
     public static void main(String[] args) {
-        Point a = new Point(1, 7);
-        Point b = new Point(12, 5);
-        double dist = a.distance(b);
+        Point first = new Point(7, 14);
+        Point second = new Point(3, 2);
+        double dist = first.distance(second);
         System.out.println(dist);
+        Point third = new Point(7, 8, 2);
+        Point fourth = new Point(4, 4, 4);
+        double dist3d = third.distance3d(fourth);
+        System.out.println(dist3d);
     }
 }
--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -1,62 +1,23 @@
 package ru.job4j.condition;
 
-import org.junit.Assert;
 import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class PointTest {
-
     @Test
-    public void when00to20then2() {
-        int expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+    public void when78And32() {
+        Point first = new Point(7, 8);
+        Point second = new Point(3, 2);
+        double result = first.distance(second);
+        assertThat(result, is(7.211102550927978));
     }
 
     @Test
-    public void when01to23then2Dot82() {
-        double expected = 2.82;
-        int x1 = 0;
-        int y1 = 1;
-        int x2 = 2;
-        int y2 = 3;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-
-    @Test
-    public void when12to46then5() {
-        int expected = 5;
-        int x1 = 1;
-        int y1 = 2;
-        int x2 = 4;
-        int y2 = 6;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-
-    @Test
-    public void when13to42then3Dot16() {
-        double expected = 3.16;
-        int x1 = 1;
-        int y1 = 3;
-        int x2 = 4;
-        int y2 = 2;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-
-    @Test
-    public void when23to02then2Dot23() {
-        double expected = 2.23;
-        int x1 = 2;
-        int y1 = 3;
-        int x2 = 0;
-        int y2 = 2;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+    public void when351And444() {
+        Point first = new Point(3, 5, 1);
+        Point second = new Point(4, 4, 4);
+        double result = first.distance3d(second);
+        assertThat(result, is(3.3166247903554));
     }
 }
